147b9c58f6f824f1f6f5bb300582d78b5de6aec5
tkachevdg <tkachevdg@gmail.com>
2023-02-09 15:57:45 +0600

    2 commit

--- Program.cs
+++ Program.cs
@@ -3,4 +3,48 @@
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 
+string[] array1 = {"Hello", "2", "world", ":-)"};
+		
+string[] ShorArray1(string[] array)
+{
+    int length = array.Length;			
+    string[] result = new string[length];
+    int count = 0;
 
+for (int i = 0; i < length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        result[count] = array[i];
+        count++;
+    }
+}
+
+Array.Resize(ref result, count);
+return result;
+}
+
+void PrintArray(string[] array)
+{
+    if(array.Length == 0)
+    {
+        Console.WriteLine("В массиве нет требуемых элементов");
+    }
+    else
+    {
+        Array.ForEach(array, (str) => Console.Write($"{str} "));
+        Console.WriteLine();
+    }
+}
+		
+void PrintTask(string[] array)
+{
+    Console.WriteLine("Дан массив:");
+    PrintArray(array);
+    string[] shortArray = ShorArray1(array);
+    Console.WriteLine($"Полученный массив из элементов с символами <= 3:");
+    PrintArray(shortArray);
+    Console.WriteLine();
+}
+		
+PrintTask(array1);
